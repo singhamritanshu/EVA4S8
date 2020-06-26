99b08461dd9146696555c42c4a9aab16ef99ea9c
Amritanshu Singh <64220796+singhamritanshu@users.noreply.github.com>
2020-06-27 00:31:26 +0530

    Update epoc.py

--- epoc.py
+++ epoc.py
@@ -32,3 +32,21 @@ for epoch in range(EPOCHS):
     test_loss , test_acc = t.test(model, device, criterion, testloader)
     testLoss.append(test_loss)
     testAcc.append(test_acc)
+def classwise_accuracy(model, device, classes, test_loader):
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+            	label = labels[i]
+            	class_correct[label] += c[i].item()
+            	class_total[label] += 1
+
+    for i in range(10):
+        print('Accuracy of %5s : %2d %%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
+classwise_accuracy(model, device, classes, testloader)
